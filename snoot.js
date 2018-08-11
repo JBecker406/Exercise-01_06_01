94ebc8a82c6da68f32068f19074d82b6792aa67d
Unknown <JBecker406@west-mec.org>
2018-08-10 18:22:06 -0700

    Added Address Validation
    Added function to validate address fieldsets which will throw an error if a field is empty and added function call for validating each address

--- snoot.js
+++ snoot.js
@@ -99,6 +99,42 @@ function setUpPage() {
     createEventListeners();
 }
 
+// function to validate address
+function validateAddress(fieldsetId) {
+    var inputElements = document.querySelectorAll("#" + fieldsetId + " input");
+    var errorDiv = document.querySelectorAll("#" + fieldsetId + " .errorMessage")[0];
+    var fieldsetValidity = true;
+    var elementCount = inputElements.length;
+    var currentElement = null;
+    try {
+        // loop required input elements
+        for (let i = 0; i < elementCount; i++) {
+            currentElement = inputElements[i];
+            // test for blank
+            if (currentElement.value === "") {
+                currentElement.style.background = "rgb(255,233,233)";
+                fieldsetValidity = false;
+            } else {
+                currentElement.style.background = "white";
+            }
+        }
+        if (fieldsetValidity === false) {
+            if (fieldsetId === "billingAddress") {
+                throw "Please complete all Billing Address information.";
+            } else {
+                throw "Please complete all Delivery Address information.";
+            }
+        } else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        }
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
+}
+
 // function to validate entire form
 function validateForm(evt) {
     if (evt.preventDefault) {
@@ -107,6 +143,9 @@ function validateForm(evt) {
     else {
         evt.returnValue = false;
     }
+
+    validateAddress("billingAddress");
+    validateAddress("deliveryAddress");
 
     if (formValidity === true) {
         document.getElementById("errorText").innerHTML = "";
